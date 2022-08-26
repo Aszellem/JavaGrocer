bbf5430b99b2608888428744f0740e9b9a229295
aszellem <aszellemek@gmail.com>
2022-08-26 15:31:26 +0200

    scanner

--- Grocer.java
+++ Grocer.java
@@ -31,6 +31,28 @@ public class Grocer {
             }
 
         }
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] store = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
+        System.out.println("\nWelcome to Java Grocers. ");
+        System.out.println("What can I help you find?\n");
+        //  Task 1: Set up Scanner, and pick up the user's response.
+        String response = scan.nextLine(); 
+
+        
+        for (int i = 0; i < store.length; i++) {
+          /* Task 2  
+                if element equals user's response  {
+                    println: \nWe have that in aisle:  <index>
+                    break;
+                }
+          */
+          if(store[i].equals(response)){
+            System.out.println("\nWe have that in aisle: " + i);
+            break;
+          }
+        }
     
     }
 }
